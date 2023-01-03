db1eac3f891b3e6b31be423ed550e53e34cf6968
likelionbootcamp3 <likelionbootcamp3@gmail.com>
2023-01-03 10:03:08 +0700

    feat: create a navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="">Shop</a>
+        </li>
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
